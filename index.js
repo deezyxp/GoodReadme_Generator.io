abda8c79f2de5842a3b9406f67dad955a3602b96
deezy <danielshiaolin@gmail.com>
2020-10-08 18:47:19 +1100

    added changes to script

--- index.js
+++ index.js
@@ -1,74 +1,90 @@
-const fs = require("fs");
-const util = require("util");
+var inquirer = require("inquirer");
+var generateMarkdown = require("./utils/generateMarkdown");
+var axios = require("axios");
+var fs = require("fs");
+// Require all npm packages and files
 
-const asyncWrite = util.promisify(fs.writeFile);
-
-// array of questions for user
-const questions = [{
-    type: "input",
-        name: "username",
-        message: "What is your GitHub user name?",
+const questions = [
+    // questions to user using "inquirer"
+    {
+      type: "input",
+      message: "Please enter your Github Username",
+      name: "username"
+    },
+  
+    {
+      type: "input",
+      message: "Please enter your project title",
+      name: "title",
+    },  
+    {
+      type: "input",
+      message: "Please enter your project description",
+      name: "description",
+    },
+  
+    {
+      type: "input",
+      message: "Please enter your project installation instructions",
+      name: "installation",
+    },
+  
+    {
+      type: "input",
+      message: "Please enter the usage of your project",
+      name: "usage",
+    },
+  
+    {
+      type: "input",
+      message: "Please enter the contribution guidelines for your project",
+      name: "contributors",
+    },
+  
+    {
+      type: "input",
+      message: "Please enter your test instructions",
+      name: "test",
     },
     {
-        type: "input",
-        name: "title",
-        message: "What is your project title?",
+        type: "list",
+        message: "Please choose a license",
+        choices: [
+            "Apache License 2.0",
+            "GNU General Public License v3.0",
+            "MIT License",
+            "BSD 2-Clause 'Simplified' License",
+            'BSD 3-Clause "New" or "Revised" License',
+            "Boost Software License 1.0",
+            "Creative Commons Zero v1.0 Universal",
+            "Eclipse Public License 2.0",
+            "GNU Affero General Public License v3.0",
+            "GNU General Public License v2.0",
+            "GNU Lesser General Public License v2.1",
+            "Mozilla Public License 2.0",
+            "The Unlicense"
+        ],
+        name: "license"
     },
-    {
-        type: "input",
-        name: "description",
-        message: "Enter your project description?",
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "Please enter your project installation instructions",
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "Provide instructions for project use",
-    },
-    {
-        type: "input",
-        name: "contribute",
-        message: "Enter the contributions associated with the project",
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "Enter your test instructions",
-    },
-    {
-        type: "input",
-        name: "license",
-        message: "Choose your license: ISC, MIT, Apache License 2.0, GNU GPLv2, GNU GPLv3",
-    },
-]
-
-function writeToFile(fileName, data) {
-    var fileName = 'README.md';
-    fs.writeFile(fileName, data,  "utf-8", function(err) {
-        if (err)  throw err;  
-        console.log("Success");
+  ];
+  
+  function init() {
+    inquirer.prompt(questions).then(answers => {
+      console.log(answers);
+      axios
+        .get("https://api.github.com/users/" + answers.username)
+        .then(response => {
+          console.log(response);
+          var imageURL = response.data.avatar_url;
+          answers.image = imageURL;
+          console.log(imageURL);
+          fs.writeFile("README.md", generateMarkdown(answers), function(err) {
+            if (err) {
+              throw err;
+            }
+          });
+        });
     });
-}
-
-
-
-// function to initialize program
-function init() {
-    inquirer.prompt(questions)
-    .then(function(data) {
-        
-        //this line of code prints the data object to the terminal
-        let readme = generateMarkdown(data);
-        var fileName = 'README.md';
-        return writeToFile(fileName, readme);
-
-    }); 
-
-}
-
-// function call to initialize program
-init();
+  }
+  
+  init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,30 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [Questions](#questions)
+  * [License](#license)
+
+  # Project Title: ${data.title}
+
+  # Username: 
+  ${data.username}
+  # Description:
+  ${data.description}
+  # Installation: 
+  ${data.installation}
+  # Usage: 
+  ${data.usage}
+  # Contribute: 
+  ${data.contribute}
+  # Tests: 
+  ${data.tests}
+  # License: 
+  ${data.license}
 
 `;
 }
